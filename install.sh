7f13a9bdb887b25b549fe21928e89e27d2cc017e
Kevin Gmelin <kgmelin11@gmail.com>
2022-04-02 17:35:34 -0400

    changes for pymavlink build on running install.sh ; SWITCH from python 3 to 2 and not minigforge distro

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-sudo MDEF=/home/wildfire/M600_ws/src/pymavlink/message_definitions /home/wildfire/miniforge3/bin/python3.9 -m pip install . -v
+sudo MDEF=/home/wildfire/M600_ws/src/pymavlink/message_definitions python2 -m pip install . -v --upgrade
