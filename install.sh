381946ce371fdfa1eae63238cd57037498cc03f2
Kevin Gmelin <kgmelin11@gmail.com>
2022-04-04 00:51:34 -0400

    Changed path to message definitions used by install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-sudo MDEF=/home/wildfire/M600_ws/src/pymavlink/message_definitions python2 -m pip install . -v --upgrade
+sudo MDEF=/home/wildfire/firefly_src/pymavlink/message_definitions python2 -m pip install . -v --upgrade
